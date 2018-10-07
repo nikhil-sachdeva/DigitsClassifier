6583d889a716f01743fc60b176d04402f4d8f355
Nikhil <Nikhil@Nikhils-MacBook-Air.local>
2018-10-08 00:22:30 +0530

    added classifier

--- app/src/main/java/com/example/nikhil/digits/DrawActivity.java
+++ app/src/main/java/com/example/nikhil/digits/DrawActivity.java
@@ -1,12 +1,19 @@
 package com.example.nikhil.digits;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.divyanshu.draw.widget.DrawView;
 
 public class DrawActivity extends AppCompatActivity {
     DrawView drawView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,14 @@ public class DrawActivity extends AppCompatActivity {
         drawView.setStrokeWidth(20);
         drawView.setColor(getResources().getColor(R.color.color_blue));
 
+
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DrawActivity.this,MainActivity.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nikhil/digits/MainActivity.java
+++ app/src/main/java/com/example/nikhil/digits/MainActivity.java
@@ -1,28 +1,53 @@
 package com.example.nikhil.digits;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.divyanshu.draw.activity.DrawingActivity;
 import com.divyanshu.draw.widget.DrawView;
-import com.joaquimley.faboptions.FabOptions;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tex;
-FabOptions fabOptions;
+public class MainActivity extends AppCompatActivity{
+
+
+    private static final int REQUEST_CODE_DRAW = 1;
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3;
+
+
 
     private static final String TAG = "ha" ;
 
@@ -32,27 +57,57 @@ FabOptions fabOptions;
         setContentView(R.layout.activity_main);
 
 
-        tex=(TextView)findViewById(R.id.tex);
-        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background);
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton2 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item2);
+        floatingActionButton3 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item3);
 
-        fabOptions=(FabOptions)findViewById(R.id.fab_options);
-        fabOptions.setButtonsMenu(R.menu.fab_menu);
-        startActivity(new Intent(MainActivity.this,DrawActivity.class));
+
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, DrawingActivity.class),REQUEST_CODE_DRAW);
+                //startActivityForResult(new Intent(MainActivity.this,DrawActivity.class),REQUEST_CODE_DRAW);
+
+            }
+        });
+        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CamActivity.class));
+
+            }
+        });
 
 
 
     }
-
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.camera){
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.d(TAG, "onActivityResult: worked");
+        byte[] result = data.getByteArrayExtra("bitmap");
+        Bitmap bitmap = BitmapFactory.decodeByteArray(result, 0, result.length);
+        try {
+            ImageClassifier imageClassifier = new ImageClassifier(MainActivity.this);
+            String ans = imageClassifier.classifyFrame(bitmap);
+            Log.d(TAG, "onActivityResult: " + ans);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(requestCode==REQUEST_CODE_DRAW){
+            if(requestCode==Activity.RESULT_OK && data!=null) {
+                Log.d(TAG, "onActivityResult: worked");
+//                byte[] result = data.getByteArrayExtra("bitmap");
+//                Bitmap bitmap = BitmapFactory.decodeByteArray(result, 0, result.length);
+//                try {
+//                    ImageClassifier imageClassifier = new ImageClassifier(MainActivity.this);
+//                    String ans = imageClassifier.classifyFrame(bitmap);
+//                    Log.d(TAG, "onActivityResult: " + ans);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+            }
 
         }
-        if(item.getItemId()==R.id.draw){
-            Toast.makeText(this, "touched", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(MainActivity.this,DrawActivity.class));
-        }
-        return super.onOptionsItemSelected(item);
+        super.onActivityResult(requestCode, resultCode, data);
     }
+
+
 }
